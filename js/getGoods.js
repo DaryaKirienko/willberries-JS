a978ea69b8c52297119f53291016a46d849ba95a
DaryaKirienko <DaryaKirienko@users.noreply.github.com>
2021-11-22 15:28:37 +0300

    fix href

--- js/getGoods.js
+++ js/getGoods.js
@@ -41,8 +41,8 @@ const getGoods = () => {
 
         localStorage.setItem('goods', JSON.stringify(arr))
 
-        if (window.location.pathname !== '/goods.html') {
-          window.location.href = '/goods.html'
+        if (window.location.pathname !== 'willberries-JS/goods.html') {
+          window.location.href = 'willberries-JS/goods.html'
         } else {
           renderGoods(arr)
         }
@@ -53,7 +53,8 @@ const getGoods = () => {
 
   const moreBtn = document.querySelector('.more')
   if (moreBtn) {
-    moreBtn.addEventListener('click', () => {
+    moreBtn.addEventListener('click', (evt) => {
+      evt.preventDefault()
       getData()
     })
   }
